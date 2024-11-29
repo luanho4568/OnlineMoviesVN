daa031e104d89de4af00d76098e30a0da218b441
lưn <luanho4568@gmail.com>
2024-11-29 22:21:59 +0700

    complete loading and handle logout

--- OnlineMoviesVN/wwwroot/js/site.js
+++ OnlineMoviesVN/wwwroot/js/site.js
@@ -33,3 +33,26 @@ function addClassLG() {
     openLg.classList.add('open');
 }
 clickShow.click(addClassLG);
+
+
+(function () {
+    window.onload = function () {
+        var preloader = document.querySelector('.cs-page-loading');
+        preloader.classList.remove('active');
+    };
+})();
+$(document).ajaxSend(function () {
+    $(".cs-page-loading").addClass("ajax_active");
+});
+$(document).ajaxComplete(function () {
+    $(".cs-page-loading").removeClass("ajax_active");
+});
+
+function ajaxLoading(loading = "on") {
+    if (loading == "on") {
+        $(".cs-page-loading").addClass("ajax_active");
+    }
+    else {
+        $(".cs-page-loading").removeClass("ajax_active");
+    }
+}
